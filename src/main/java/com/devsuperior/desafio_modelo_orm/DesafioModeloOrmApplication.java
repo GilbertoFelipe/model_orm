dd70546e4c95643ff3cf10aa9fd6423c0d5a732f
Gilberto Felipe <gilbertofelipe.junior@gmail.com>
2024-10-23 11:05:30 -0300

    Deasfio modelo ORM

---
+++ src/main/java/com/devsuperior/desafio_modelo_orm/DesafioModeloOrmApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.desafio_modelo_orm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioModeloOrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioModeloOrmApplication.class, args);
+	}
+
+}
